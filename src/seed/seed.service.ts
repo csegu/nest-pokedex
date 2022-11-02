cc427b28668bc0dca61b199ec53e8c9bc50eef09
csegura <cristian.seguraguerrero@telefonica.com>
2022-11-02 15:14:40 +0100

    Antes de insertar por lote

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,23 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+@Injectable()
+export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+    data.results.forEach( ({name, url}) => {
+      const segments = url.split('/');
+      const no:number = +segments[segments.length - 2];
+      console.log({name, no});
+    })
+    return data.results;
+  }
+}
